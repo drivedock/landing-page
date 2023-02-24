8e7016fabe213748200b4d3f7238fb68534126a4
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2023-02-24 20:58:08 +0530

    handled landing responsive

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -6,7 +6,7 @@ import Qoutes from "../assets/quotes.png";
 function LandingPage() {
   return (
     <section className="landingPage display__FlexColumn alignCenter">
-      <article className="container display__Flex alignCenter justifyCenter">
+      <article className="container display__Flex alignCenter justifyCenter column__Flex">
         <figure className="flex30 image_container">
           <img
             style={{
@@ -20,27 +20,30 @@ function LandingPage() {
         </figure>
 
         <div className="flex50 LandingRightContent">
-         <div className=" logo-container display__flexColumn">
-              <img
-                style={{ width: "20%" }}
-                src={ProductLogo}
-                alt="Product Logo"
-              />
-              <h2 className="company-name">DriveDock Groups</h2>{" "}
-         </div>
-          <h1 className="heading"> <span className="glass">Presentig</span> Ourseleves</h1>
+          <div className=" logo-container display__flexColumn">
+            <img
+              style={{ width: "20%" }}
+              src={ProductLogo}
+              alt="Product Logo"
+            />
+            <h2 className="company-name">DriveDock Groups</h2>{" "}
+          </div>
+          <h1 className="heading">
+            {" "}
+            <span className="glass">Presentig</span> Ourseleves
+          </h1>
           <p className="landingContent">
             Education in a practicel way we always wanted!
           </p>
           <p className="landingContent">
-            We're going to be going <span className="glass">live</span>on this website soon, so mark your
-            calendars and get ready to join us for an unforgettable online
-            experience!
+            We're going to be going <span className="glass">live</span>on this
+            website soon, so mark your calendars and get ready to join us for an
+            unforgettable online experience!
           </p>
         </div>
       </article>
       <footer className="footer">
-      <small className="footer-glass">&copy; DriveDock Groups @ 2023</small>
+        <small className="footer-glass">&copy; DriveDock Groups @ 2023</small>
       </footer>
     </section>
   );
